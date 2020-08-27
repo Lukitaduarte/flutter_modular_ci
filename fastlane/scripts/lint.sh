87fb5016763c3cf4cca0ec481efac4cbc35f1c56
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-08-27 11:10:44 -0300

    initial commit

---
+++ fastlane/scripts/lint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+error=false
+
+cd ..
+
+# run flutter lint and set error flag 
+# if an issue in lint is founded
+flutter analyze || error=true
+
+# fail build if there was an error
+if [[ ${error} = true ]];
+then
+    exit -1
+fi
