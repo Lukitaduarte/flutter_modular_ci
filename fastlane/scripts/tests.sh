857e9e43b822c868829bddf1015eaf41559f663d
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-08-31 20:10:24 -0300

    Update tests.sh

--- fastlane/scripts/tests.sh
+++ fastlane/scripts/tests.sh
@@ -9,27 +9,46 @@ runTests () {
 
   cd ${package_dir};
 
-  if [[ -f "pubspec.yaml" ]] && [[ -d "test" ]]; then
+  if [[ -f "pubspec.yaml" ]]; then
     # install flutter dependencies
     flutter packages get
 
-    # run flutter tests
-    if grep flutter pubspec.yaml > /dev/null; then
+    if [[ -d "test" ]]; then
+
+      escapedPath="$(echo $1 | sed 's/\//\\\//g')"
+
+      # run flutter tests
+      if grep flutter pubspec.yaml > /dev/null; then
+
         echo "Running flutter tests in $1"
-        flutter test || error=true
-    else
-      # run dart tests
-      echo "Running dart tests..."
-      pub get
-      pub run test || error=true
+        if [ -f "test/all_tests.dart" ]; then
+          flutter test --coverage test/all_tests.dart --concurrency=4 || error=true
+        else
+          flutter test --coverage --concurrency=4 || error=true
+        fi
+
+        if [ -d "coverage" ]; then
+          # combine line coverage info from package tests to a common file
+          sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> $2/lcov.info
+          rm -rf "coverage"
+        fi
+
+      else
+        # run dart tests
+        echo "Running dart tests..."
+        pub get
+        pub run test || error=true
+      fi
+
     fi
+
   fi
 
   cd - > /dev/null
 }
 
 repo_dir=`pwd`
-package_dirs=(`find . -maxdepth 2 -type d`)
+package_dirs=(`find . -maxdepth 3 -type d`)
 for package_dir in "${package_dirs[@]}"; do
     runTests ${package_dir} ${repo_dir}
 done
