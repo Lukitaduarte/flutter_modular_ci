4911dcb3c861fcd2476fb1371b0618d5eba80a37
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-09-09 00:21:53 -0300

    Update coverage.sh

--- fastlane/scripts/coverage.sh
+++ fastlane/scripts/coverage.sh
@@ -5,7 +5,23 @@ coverage=$(lcov --summary lcov.info)
 
 [[ ${coverage} =~ $regex ]]
 
-echo "# flutter_modular_ci [![Coverage](https://badgen.net/badge/coverage/${BASH_REMATCH[0]}/green?icon=github)](https://badgen.net/badge/coverage/59,9%25/green?icon=github)" > coverage.txt
+echo "# flutter_modular_ci [![Coverage](https://badgen.net/badge/coverage/${BASH_REMATCH[0]}/green?icon=github)](https://badgen.net/badge/coverage/59,9%25/green?icon=github)
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+" > coverage.txt
 
 awk '{sub("%","%25")}1' coverage.txt > temp.txt && mv temp.txt coverage.txt
 
