ab692dc59060a7ce31e12de60c6da522e693371e
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-09-08 23:33:31 -0300

    Update coverage.sh

--- fastlane/scripts/coverage.sh
+++ fastlane/scripts/coverage.sh
@@ -5,8 +5,8 @@ coverage=$(lcov --summary lcov.info)
 
 [[ ${coverage} =~ $regex ]]
 
-readme="# flutter_modular_ci [![Coverage](https://badgen.net/badge/coverage/${BASH_REMATCH[0]}/green?icon=github)](https://badgen.net/badge/coverage/59,9%25/green?icon=github)" > coverage.txt
+echo "# flutter_modular_ci [![Coverage](https://badgen.net/badge/coverage/${BASH_REMATCH[0]}/green?icon=github)](https://badgen.net/badge/coverage/59,9%25/green?icon=github)" > coverage.txt
 
-awk '{sub("%","%25")}' $readme > coverage.txt
+awk '{sub("%","%25")}' coverage.txt > coverage.txt
 
 # printf "|\t Code Coverage \t\t\t|\n|\t Development coverage \t| %s |\n|\t Your PR coverage \t| %s |" ${BASH_REMATCH[0]} ${BASH_REMATCH[0]}
