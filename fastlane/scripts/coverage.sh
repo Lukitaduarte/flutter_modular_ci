c65b75252341834a0559b29373a3e251e8689cd7
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-09-09 12:42:40 -0300

    fix coverage

--- fastlane/scripts/coverage.sh
+++ fastlane/scripts/coverage.sh
@@ -5,11 +5,10 @@ coverage=$(lcov --summary lcov.info)
 
 [[ ${coverage} =~ $regex ]]
 
-readme=$(<README.md)
 percent="${BASH_REMATCH[0]}%25"
 
 echo "[![Coverage](https://badgen.net/badge/coverage/$percent/green?icon=github)](https://badgen.net/badge/coverage/59,9%25/green?icon=github)" > coverage.txt
 
-awk '{sub("{percent}","$percent")}1' $readme > temp.md && mv temp.md README.md
+awk '{sub("{percent}","$percent")}1' README.md > temp.md && mv temp.md README.md
 
 # printf "|\t Code Coverage \t\t\t|\n|\t Development coverage \t| %s |\n|\t Your PR coverage \t| %s |" ${BASH_REMATCH[0]} ${BASH_REMATCH[0]}
